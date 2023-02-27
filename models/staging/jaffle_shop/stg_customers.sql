0358cd5ff81df65e41ecd331cf0c9991a855c43d
FunchadelicAD <adryanf@gmail.com>
2023-02-27 22:51:13 +0000

    Created src yml. Modified stg views to reference src_jaffle_shop

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -1,9 +1,14 @@
-with customers as (
+with source as (
+    select * 
+    from {{ source('jaffle_shop', 'customers')}}
+),
+
+customers as (
     select 
         id as customer_id, 
         first_name,
         last_name
-    from raw.jaffle_shop.customers
+    from source
 )
 
 select *
